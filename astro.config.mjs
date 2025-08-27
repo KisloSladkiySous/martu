1e3e4a8e50b63fcc5abc216b81745b17f36559db
Viktor Skomorohov <v.skomorohov@teleformis.ru>
2025-08-28 01:34:57 +0300

    Martu about section

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://kislosladkiysous.github.io",
   base: "/martu",
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
 });
