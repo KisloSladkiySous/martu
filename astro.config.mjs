a1e53fe1b75cb50fcc6f78a04edcb8789dd64ade
Viktor Skomorohov <v.skomorohov@teleformis.ru>
2025-08-28 19:42:43 +0300

    Martu/jsx added solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://kislosladkiysous.github.io",
   base: "/martu",
+
   prefetch: {
     defaultStrategy: "viewport",
   },
+
+  integrations: [solidJs()],
 });
