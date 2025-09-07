84941612b6b290adc456a186f36bff7fe4b97c4f
Viktor Skomorohov <v.skomorohov@teleformis.ru>
2025-09-07 18:28:10 +0300

    Martu adaptive hueptive

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kislosladkiysous.github.io",
+<<<<<<< Updated upstream
   base: "/",
+=======
+  base: "/martu/",
+>>>>>>> Stashed changes
 });
