d1045cfc7a34a8890c86aadc1b515682afe15641
Zazik <zaza.haratishvili@gmail.com>
2025-09-08 17:51:51 +0300

    underline animation in navigation link

--- src/social-links.ts
+++ src/social-links.ts
@@ -1,3 +1,8 @@
 export const socialLinks = {
   inst: "https://www.instagram.com/martu.agency/",
+  vk: "https://vk.com/martu.agency",
+  tel: {
+    text: "+7 (999) 985-10-83",
+    url: "tel:+79999851083",
+  },
 };
