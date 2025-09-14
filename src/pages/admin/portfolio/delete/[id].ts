21237f32b0af8b79d3fc649003c09ed894fd8f35
Viktor Skomorohov <v.skomorohov@teleformis.ru>
2025-09-14 22:21:34 +0300

    martu comment admin panel

--- src/pages/admin/portfolio/delete/[id].ts
+++ src/pages/admin/portfolio/delete/[id].ts
@@ -1,28 +1,28 @@
-import type { APIRoute } from "astro";
-import { db, Group, Media, eq } from "astro:db";
+// import type { APIRoute } from "astro";
+// import { db, Group, Media, eq } from "astro:db";
 
-export const POST: APIRoute = async ({ params }) => {
-  const id = Number(params.id);
+// export const POST: APIRoute = async ({ params }) => {
+//   const id = Number(params.id);
 
-  if (isNaN(id)) {
-    return new Response(JSON.stringify({ error: "Неверный ID" }), {
-      status: 400,
-    });
-  }
+//   if (isNaN(id)) {
+//     return new Response(JSON.stringify({ error: "Неверный ID" }), {
+//       status: 400,
+//     });
+//   }
 
-  // Удаляем медиа группы
-  await db.delete(Media).where(eq(Media.groupId, id));
+//   // Удаляем медиа группы
+//   await db.delete(Media).where(eq(Media.groupId, id));
 
-  // Удаляем саму группу
-  await db.delete(Group).where(eq(Group.id, id));
+//   // Удаляем саму группу
+//   await db.delete(Group).where(eq(Group.id, id));
 
-  // Перенаправляем обратно в админку
-  return new Response(null, {
-    status: 303,
-    headers: {
-      Location: "/admin",
-    },
-  });
-};
+//   // Перенаправляем обратно в админку
+//   return new Response(null, {
+//     status: 303,
+//     headers: {
+//       Location: "/admin",
+//     },
+//   });
+// };
 
-export const prerender = false;
+// export const prerender = false;
--- src/pages/admin/reviews/delete/[id].ts
+++ src/pages/admin/reviews/delete/[id].ts
@@ -1,14 +1,14 @@
-import type { APIRoute } from "astro";
-import { db, Reviews, eq } from "astro:db";
+// import type { APIRoute } from "astro";
+// import { db, Reviews, eq } from "astro:db";
 
-export const POST: APIRoute = async ({ params }) => {
-  const id = Number(params.id);
-  if (isNaN(id)) return new Response("Неверный ID", { status: 400 });
+// export const POST: APIRoute = async ({ params }) => {
+//   const id = Number(params.id);
+//   if (isNaN(id)) return new Response("Неверный ID", { status: 400 });
 
-  await db.delete(Reviews).where(eq(Reviews.id, id));
+//   await db.delete(Reviews).where(eq(Reviews.id, id));
 
-  return new Response(null, {
-    status: 303,
-    headers: { Location: "/admin/reviews" },
-  });
-};
+//   return new Response(null, {
+//     status: 303,
+//     headers: { Location: "/admin/reviews" },
+//   });
+// };
