121ecb1b747f8c407e9430afb2d68635ace71657
ViktorSkomorokhov <sladkiysous@gmail.com>
2025-09-17 20:29:27 +0300

    envelope

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kislosladkiysous.github.io",
-  base: "/martu",
   devToolbar: { enabled: false },
-  output: "static",
+
   // integrations: [db()],
 });
