5af01b26a62cb5d94031fbd16c6250eceec51f49
Zazik <zaza.haratishvili@gmail.com>
2025-09-23 21:06:13 +0300

    fully removed db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import db from "@astrojs/db";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://martuagency.ru",
   devToolbar: { enabled: false },
   // output: "static",
-  // integrations: [db()],
 });
