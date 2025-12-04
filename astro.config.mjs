ee7844386ee6fde07e18a4801172ea87eccca2a0
Viktor Skomorohov <v.skomorohov@teleformis.ru>
2025-12-04 19:03:31 +0300

    added node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   site: "https://martuagency.ru",
   devToolbar: { enabled: false },
+  adapter: node({
+    mode: "standalone",
+  }),
   image: {
     domains: ["cms.agencymartu.ru", "127.0.0.1:1337"],
   },
